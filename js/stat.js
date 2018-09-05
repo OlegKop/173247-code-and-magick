83611ec8f4534242e6511322d2df2e565c700004
Копытин Олег Николаевич <o.kopytin@hq.qiwi.com>
2018-09-05 16:39:02 +0300

    в vs/stat.js разделил ф-цию renderStatistics на мелкие

--- js/stat.js
+++ js/stat.js
@@ -21,6 +21,12 @@ var renderCloud = function (ctx, x, y, color) {
   ctx.fillRect(x, y, cloud.WIDTH, cloud.HEIGHT);
 };
 
+var renderTitle = function (ctx, texts, x, y, color) {
+  ctx.font = '16px PT Mono';
+  ctx.fillStyle = color;
+  ctx.fillText(texts, x, y);
+};
+
 var getMaxElement = function (arr) {
   var maxElement = arr[0];
 
@@ -33,50 +39,72 @@ var getMaxElement = function (arr) {
   return maxElement;
 };
 
+var colorRgb = {
+  RED: 0,
+  GREEN: 0,
+  BLUE: 255
+};
+
+var randomColor = function () {
+  var red = colorRgb.RED;
+  var green = colorRgb.GREEN;
+  var blue = colorRgb.BLUE;
+  if (red === 0) {
+    red = Math.floor(Math.random() * (256))
+    ;
+  }
+  if (green === 0) {
+    green = Math.floor(Math.random() * (256));
+  }
+  if (blue === 0) {
+    blue = Math.floor(Math.random() * (256));
+  }
+
+  return '#' + red.toString(16) + green.toString(16) + blue.toString(16);
+};
+
+var getRandomColor = function (ctx, names) {
+  if (names === 'Вы') {
+    ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+  } else {
+    ctx.fillStyle = randomColor();
+  }
+};
+
+var renderColumn = function (ctx, x, y, width, height) {
+  ctx.fillRect(x, y, width, height);
+};
+
+var renderTimeTotal = function (ctx, times, x, y, color) {
+  ctx.fillStyle = color;
+  ctx.fillText(Math.round(times), x, y);
+};
+
+var renderPlayer = function (ctx, players, x, y, color) {
+  ctx.fillStyle = color;
+  ctx.fillText(players, x, y);
+};
+
+var renderChart = function (ctx, names, x, y, width, height) {
+  getRandomColor(ctx, names);
+  renderColumn(ctx, x, y, width, height);
+};
+
 window.renderStatistics = function (ctx, names, times) {
   renderCloud(ctx, cloud.X + cloud.GAP, cloud.Y + cloud.GAP, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, cloud.X, cloud.Y, '#fff');
-  ctx.font = '16px PT Mono';
-  ctx.fillStyle = '#000';
-  ctx.fillText('Ура вы победили!', 110, 50);
-  ctx.fillText('Список результатов:', 110, 70);
+  renderTitle(ctx, 'Ура вы победили!', cloud.X + cloud.GAP, cloud.Y * 5, '#000');
+  renderTitle(ctx, 'Список результатов:', cloud.X + cloud.GAP, cloud.Y * 7, '#000');
   var maxTime = getMaxElement(times);
-  var colorRgb = {
-    RED: 0,
-    GREEN: 0,
-    BLUE: 255};
-  var randColor = function () {
-    var red = colorRgb.RED;
-    var green = colorRgb.GREEN;
-    var blue = colorRgb.BLUE;
-    if (red === 0) {
-      red = Math.floor(Math.random() * (256))
-      ;
-    }
-    if (green === 0) {
-      green = Math.floor(Math.random() * (256));
-    }
-    if (blue === 0) {
-      blue = Math.floor(Math.random() * (256));
-    }
-
-    return '#' + red.toString(16) + green.toString(16) + blue.toString(16);
-  };
   var timeTotal = function () {
     var Total = Math.round((barHeight * times[i]) / maxTime);
     return Total;
   };
+
   for (var i = 0; i < names.length; i++) {
-    ctx.fillStyle = '#000';
-    ctx.fillText(names[i], cloud.X + gistograma.WIDTH + (gistograma.FONT + gistograma.WIDTH) * i, cloud.Y + gistograma.HEIGHT + gistograma.FONT * 2);
-    if (names[i] === 'Вы') {
-      ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-    } else {
-      ctx.fillStyle = randColor(colorRgb);
-    }
-    ctx.fillRect(cloud.X + gistograma.WIDTH + (gistograma.FONT + gistograma.WIDTH) * i, cloud.Y - cloud.GAP * 2 + gistograma.HEIGHT + gistograma.FONT * 2, gistograma.WIDTH, -timeTotal());
-    ctx.fillStyle = '#000';
-    ctx.fillText(Math.round(times[i]), cloud.X + gistograma.WIDTH + (gistograma.FONT + gistograma.WIDTH) * i, cloud.HEIGHT - timeTotal() - gistograma.FONT);
+    renderPlayer(ctx, names[i], cloud.X + gistograma.WIDTH + (gistograma.FONT + gistograma.WIDTH) * i, cloud.Y + gistograma.HEIGHT + gistograma.FONT * 2, '#000');
+    renderChart(ctx, names[i], cloud.X + gistograma.WIDTH + (gistograma.FONT + gistograma.WIDTH) * i, cloud.Y - cloud.GAP * 2 + gistograma.HEIGHT + gistograma.FONT * 2, gistograma.WIDTH, -timeTotal());
+    renderTimeTotal(ctx, times[i], cloud.X + gistograma.WIDTH + (gistograma.FONT + gistograma.WIDTH) * i, cloud.HEIGHT - gistograma.FONT - timeTotal(), '#000');
   }
 };
 
