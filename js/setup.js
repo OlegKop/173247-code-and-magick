fcb5afb35ba0f6aa61490105445037e151580d25
Копытин Олег Николаевич <o.kopytin@hq.qiwi.com>
2018-09-08 20:44:18 +0300

    добавлен файл vs/setup.js - настройка персонажа

---
+++ js/setup.js
@@ -0,0 +1,68 @@
+'use strict';
+
+var userDialog = document.querySelector('.setup');
+userDialog.classList.remove('hidden');
+
+var similarListElement = userDialog.querySelector('.setup-similar-list');
+
+var similarWizardTemplate = document.querySelector('#similar-wizard-template')
+    .content
+    .querySelector('.setup-similar-item');
+
+
+
+var firstname = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+
+var lastName = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+
+var coatColor = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+
+var eyesColor = ['black', 'red', 'blue', 'yellow', 'green'];
+
+var randomElement = function (element) {
+  var element = element[Math.floor(Math.random() * element.length)];
+  return element;
+};
+
+var getRandomWizards = function (magicName, magicLastName, magicCoatColor, magicEyesColor) {
+    var magics = [];
+      for (var j = 1; j <= 5; j++) {
+      magics.push(magic);
+
+    var firstNameRemove = magicName.splice(magicName.indexOf(firstName),1);
+    var lastNameRemove = magicLastName.splice(magicLastName.indexOf(lastName),1);    
+    var coatColorRemove = magicCoatColor.splice(magicCoatColor.indexOf(coatColor),1);
+    var eyesColorRemove = magicEyesColor.splice(magicEyesColor.indexOf(eyesColor),1);
+    
+    for (var i = 0; i <= j; i++) {
+      var firstName = randomElement(magicName);
+      var lastName = randomElement(magicLastName);
+      var name = firstName + ' ' + lastName;
+      var coatColor = randomElement(magicCoatColor);
+      var eyesColor = randomElement(magicEyesColor);    
+      var magic = {name, coatColor, eyesColor};
+      
+          }
+      }  
+      magics.splice(0,1);
+    return magics;
+  };
+var wizards = getRandomWizards(firstname, lastName, coatColor, eyesColor);
+
+var renderWizard = function (wizard) {
+    var wizardElement = similarWizardTemplate.cloneNode(true);
+    
+    wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
+    wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
+    wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
+    
+    return wizardElement;
+  }
+  
+  var fragment = document.createDocumentFragment();
+  for (var i = 0; i < wizards.length; i++) {
+    fragment.appendChild(renderWizard(wizards[i]));
+  }
+  similarListElement.appendChild(fragment);
+  
+  userDialog.querySelector('.setup-similar').classList.remove('hidden');
